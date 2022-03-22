6ae27e1be657448382a92f96d9af4c7e6f09b942
RaptureAwaits <fredericsmythe512@gmail.com>
2022-03-22 14:54:38 +0000

    Configured basic templates

--- tango_with_django_project/course_app/views.py
+++ tango_with_django_project/course_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'course_app/index.html')
+
+def courses(request):
+    return render(request, 'course_app/courses.html')
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from course_app import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('courses/', views.courses, name='courses'),
     path('admin/', admin.site.urls),
 ]
